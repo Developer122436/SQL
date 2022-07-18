6c701b7fa3f648e57c490b89b913ece2015c91e7
Dima <dimaspektor12@gmail.com>
2022-07-18 15:36:07 +0300

    Update TrainingQuery.sql

--- SubQuery/TrainingQuery.sql
+++ SubQuery/TrainingQuery.sql
@@ -4,7 +4,7 @@
 
   SET @sDateCalls='2021-02-01'
   SET @eDateCalls='2021-02-28'
-  SET @MokedX='ש.ל.ה';
+  SET @MokedX='׳©.׳.׳”';
 
 WITH cte as
 (
@@ -12,8 +12,8 @@ WITH cte as
 	UNION ALL
 	SELECT DATEADD(DAY,-1,n) FROM cte WHERE DATEADD(dd,-1,n)> DATEADD(MONTH, DATEDIFF(MONTH, 0, @sDateCalls)-1, 0)
 )
-SELECT CAST(n AS DATE) AS 'תאריך'
-, ISNULL(SUM(ROUND(CAST(DATEDIFF(SECOND, TAL.login, TAL.logout) AS Float)/3600,2)),0) AS 'שעות הדרכה'
+SELECT CAST(n AS DATE) AS '׳×׳׳¨׳™׳'
+, ISNULL(SUM(ROUND(CAST(DATEDIFF(SECOND, TAL.login, TAL.logout) AS Float)/3600,2)),0) AS '׳©׳¢׳•׳× ׳”׳“׳¨׳›׳”'
 FROM (
 	  SELECT * 
       FROM cte
@@ -32,15 +32,15 @@ WITH cte as
 	union all
 	SELECT dateadd(DAY,-1,n) from cte where dateadd(dd,-1,n)> DATEADD(MONTH, DATEDIFF(MONTH, 0, GETDATE())-1, 0)
 )
-SELECT CAST(n AS DATE) AS 'תאריך'
-, ISNULL(SUM(ROUND(CAST(DATEDIFF(SECOND, TAL.login, TAL.logout) AS Float)/3600,2)),0) AS 'שעות הדרכה'
+SELECT CAST(n AS DATE) AS '׳×׳׳¨׳™׳'
+, ISNULL(SUM(ROUND(CAST(DATEDIFF(SECOND, TAL.login, TAL.logout) AS Float)/3600,2)),0) AS '׳©׳¢׳•׳× ׳”׳“׳¨׳›׳”'
 FROM (
 	  SELECT * 
       FROM cte
       WHERE CAST(n AS DATE) BETWEEN DATEADD(MONTH, DATEDIFF(MONTH, 0, GETDATE())-1, 0) AND DATEADD(MONTH, DATEDIFF(MONTH, -1, GETDATE())-1, -1)) AllDates
 LEFT JOIN ( SELECT *
 			FROM [dbo].[TrainingAgentsLogin]
-			WHERE ProjectX IN ('ש.ל.ה באר שבע') ) TAL ON CAST(TAL.DateX AS DATE) = CAST(AllDates.n AS DATE)
+			WHERE ProjectX IN ('׳©.׳.׳” ׳‘׳׳¨ ׳©׳‘׳¢') ) TAL ON CAST(TAL.DateX AS DATE) = CAST(AllDates.n AS DATE)
 GROUP BY CAST(AllDates.n AS DATE)
 ORDER BY CAST(AllDates.n AS DATE) 
 
@@ -50,9 +50,9 @@ ORDER BY CAST(AllDates.n AS DATE)
   SET @sDateAgent='2022-03-01'
   SET @eEndAgent='2022-03-31'
 
-SELECT DateX, SUM(ROUND(CAST(DATEDIFF(second, LogIn, LogOut) AS Float)/3600,2))  AS 'שעות הדרכה'
+SELECT DateX, SUM(ROUND(CAST(DATEDIFF(second, LogIn, LogOut) AS Float)/3600,2))  AS '׳©׳¢׳•׳× ׳”׳“׳¨׳›׳”'
 FROM [RepGateDB].[dbo].[TrainingAgentsLogin]
-WHERE ProjectX IN ('ש.ל.ה') AND DateX BETWEEN @sDateAgent AND @eEndAgent
+WHERE ProjectX IN ('׳©.׳.׳”') AND DateX BETWEEN @sDateAgent AND @eEndAgent
 GROUP BY DateX, ProjectX
 ORDER BY DateX DESC
 
@@ -64,9 +64,9 @@ ORDER BY DateX DESC
   SET @sDateAgent='2022-03-01'
   SET @eEndAgent='2022-03-31'
 
-SELECT DateX, SUM(ROUND(CAST(DATEDIFF(second, LogIn, LogOut) AS Float)/3600,2))  AS 'שעות הדרכה'
+SELECT DateX, SUM(ROUND(CAST(DATEDIFF(second, LogIn, LogOut) AS Float)/3600,2))  AS '׳©׳¢׳•׳× ׳”׳“׳¨׳›׳”'
 FROM [RepGateDB].[dbo].[TrainingAgentsLogin]
-WHERE ProjectX IN ('ש.ל.ה באר שבע') AND DateX BETWEEN @sDateAgent AND @eEndAgent
+WHERE ProjectX IN ('׳©.׳.׳” ׳‘׳׳¨ ׳©׳‘׳¢') AND DateX BETWEEN @sDateAgent AND @eEndAgent
 GROUP BY DateX, ProjectX
 ORDER BY DateX DESC
 
